6d39e6878e5b25eeee6a487c89d4a6074cf77199
Michal-prog <mnowakowski92@gmail.com>
2021-05-11 20:26:46 +0200

    Create tables

---
+++ src/Codecool.CodecoolShop/Daos/Context/Context.cs
@@ -0,0 +1,19 @@
+﻿using Codecool.CodecoolShop.Models;
+using Microsoft.EntityFrameworkCore;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace Codecool.CodecoolShop.Daos.Conte
+{
+    public class Context: DbContext
+    {
+        public Microsoft.EntityFrameworkCore.DbSet<Product> Products { get; set; }
+        public Context(DbContextOptions<Context>opt) : base(opt)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().Property(e => e.DefaultPrice).HasPrecision(38, 18);
+        }
+    }
+}
--- src/Codecool.CodecoolShop/Startup.cs
+++ src/Codecool.CodecoolShop/Startup.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Codecool.CodecoolShop.Daos;
+using Codecool.CodecoolShop.Daos.Conte;
 using Codecool.CodecoolShop.Daos.Implementations;
 using Codecool.CodecoolShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,7 +17,7 @@ using Microsoft.Extensions.Hosting;
 namespace Codecool.CodecoolShop
 {
     public class Startup
-    {
+    {  
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -28,6 +30,8 @@ namespace Codecool.CodecoolShop
         {
             services.AddControllersWithViews();
             services.AddSession();
+            services.AddDbContext<Context>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
