88b0ed562a134120aa6f143bb7fee7211bda7828
Tomasz Straszewski <tomasz.sstraszewski@gmail.com>
2021-05-14 19:17:31 +0200

    Add checkout html

--- src/Codecool.CodecoolShop/Models/Checkout.cs
+++ src/Codecool.CodecoolShop/Models/Checkout.cs
@@ -8,6 +8,6 @@
         public string BillingAdress { get; set; }
         public string Country { get; set; }
         public string Zipcode { get; set; }
-        public string Adress { get; set; }
+        public string Adress { get; set; 
     }
 }
